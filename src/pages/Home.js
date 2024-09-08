0befd159a05cef371a88fb287b7127fefe9e9529
Zakwan Hamid <zakwanhamid@gmail.com>
2024-09-08 11:51:06 +0800

    add users buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,6 +37,11 @@ export default function Home() {
                 <td>{user.name}</td>
                 <td>{user.username}</td>
                 <td>{user.email}</td>
+                <td>
+                    <button className='btn btn-primary mx-2 '>View</button>
+                    <button className='btn btn-outline-primary mx-2 '>Edit</button>
+                    <button className='btn btn-danger mx-2 '>Delete</button>
+                </td>
                 </tr>
             ))}
             </tbody>
